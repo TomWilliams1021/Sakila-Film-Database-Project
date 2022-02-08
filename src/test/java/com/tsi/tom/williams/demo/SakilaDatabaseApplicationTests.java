3a7b406a33fde151a8654c4f2362d19fa44e78fd
TomWilliams1021 <tomwilliams1021@gmail.com>
2022-02-08 15:01:31 +0000

    Initial Project Setup

---
+++ src/test/java/com/tsi/tom/williams/demo/SakilaDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsi.tom.williams.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakilaDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
