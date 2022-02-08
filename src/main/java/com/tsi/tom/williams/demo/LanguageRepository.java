f03a7100aef9b488b3ce3bf30ebed51cea7e7c40
TomWilliams1021 <tomwilliams1021@gmail.com>
2022-02-08 17:25:17 +0000

    First step of pulling in data from database, pull in language table and enable local host pure text display of this data

---
+++ src/main/java/com/tsi/tom/williams/demo/LanguageRepository.java
@@ -0,0 +1,7 @@
+package com.tsi.tom.williams.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface LanguageRepository extends CrudRepository<Language, Integer> {
+
+}
--- src/main/java/com/tsi/tom/williams/demo/SakilaDatabaseApplication.java
+++ src/main/java/com/tsi/tom/williams/demo/SakilaDatabaseApplication.java
@@ -1,23 +1,31 @@
 package com.tsi.tom.williams.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("/Home")
 public class SakilaDatabaseApplication {
+
+	@Autowired
+	private LanguageRepository languageRepository;
+
+	public SakilaDatabaseApplication(LanguageRepository languageRepository){
+		this.languageRepository = languageRepository;
+	}
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(SakilaDatabaseApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+	@GetMapping("/AllLanguages")
+	public @ResponseBody
+	Iterable<Language> GetAllLanguages(){
+		return languageRepository.findAll();
 	}
 
 	public static boolean testTest(){
