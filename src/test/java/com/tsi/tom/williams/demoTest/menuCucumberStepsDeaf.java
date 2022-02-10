742a6f3a8590488ca3d26b0e03dcf8def51b6655
TomWilliams1021 <tomwilliams1021@gmail.com>
2022-02-10 15:30:24 +0000

    Added first cucumber test, checking wether a language is addedto the language table.

--- src/test/java/com/tsi/tom/williams/demoTest/menuCucumberStepsDeaf.java
+++ src/test/java/com/tsi/tom/williams/demoTest/menuCucumberStepsDeaf.java
@@ -1,25 +1,62 @@
 package com.tsi.tom.williams.demoTest;
 
+import com.tsi.tom.williams.demo.*;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
+@ExtendWith(MockitoExtension.class)
 public class menuCucumberStepsDeaf {
-    /*
-    @Given("I have chosen a film")
-    public void i_have_chosen_a_film() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+    private SakilaDatabaseApplication sakilaDBApp;
+
+    @Mock
+    private LanguageRepository languageRepository;
+    @Mock
+    private ActorRepository actorRepository;
+    @Mock
+    private CategoryRepository categoryRepository;
+    @Mock
+    private FilmRepository filmRepository;
+
+    @BeforeEach
+    void setup() {
+        languageRepository = mock(LanguageRepository.class);
+        actorRepository = mock(ActorRepository.class);
+        categoryRepository = mock(CategoryRepository.class);
+        filmRepository = mock(FilmRepository.class);
+
+        sakilaDBApp = new SakilaDatabaseApplication(languageRepository, actorRepository, categoryRepository, filmRepository);
     }
-    @When("I ask if language is {string}")
-    public void i_ask_if_language_is(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+    Language storedLanguage;
+    String actualResult;
+    @Given("We have a language entry to add")
+    public void we_have_a_language_entry_to_add() {
+        setup();
+        storedLanguage = new Language("Language Test");
     }
-    @Then("I should be told {string}")
-    public void i_should_be_told(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    @When("We add the language entry")
+    public void we_add_the_language_entry() {
+        actualResult = sakilaDBApp.addLanguage(storedLanguage.getName());
     }
-    */
+    @Then("The language should be added and we should return that the new language was saved")
+    public void The_language_should_be_added() {
+        String expectedResult = "Saved";
+        Assertions.assertEquals(expectedResult, actualResult, "Save new language failed.");
+
+        ArgumentCaptor<Language> languageArgumentCaptor = ArgumentCaptor.forClass(Language.class);
+        verify(languageRepository).save(languageArgumentCaptor.capture());
+        languageArgumentCaptor.getValue();
+    }
+
 }
--- src/test/resources/Cucumber/Something.feature
+++ src/test/resources/Cucumber/Something.feature
@@ -1,7 +1,5 @@
-#Feature: Is the language Spanish?
-#  People want to know if it's Spanish
-#  Scenario: Language is not Spanish
-#    Given I have chosen a film
-#    When I ask if language is "Spanish"
-#    Then I should be told "No"
-
+Feature: There's an entry to add
+  Scenario: Add a language
+    Given We have a language entry to add
+    When We add the language entry
+    Then The language should be added and we should return that the new language was saved
